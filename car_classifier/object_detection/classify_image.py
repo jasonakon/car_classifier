3083728a0ccad2be1efd4ed93ffdcf87c07e595e
Jason Lim <jasonakon@gmail.com>
2019-06-16 20:46:55 +0800

    several amendment

--- car_classifier/object_detection/classify_image.py
+++ car_classifier/object_detection/classify_image.py
@@ -13,12 +13,12 @@ from PIL import ImageFont, ImageDraw
 #Path to your test image:
 PATH_TO_TEST_IMAGES_DIR = 'test_images'
 #Path to your model:
-MODEL_NAME = 'trained_models\grab_50k'
+MODEL_NAME = os.path.join("trained_models","grab_model")
 CWD_PATH = os.getcwd()
 #Path to your inference graph:
-PATH_TO_CKPT = os.path.join(CWD_PATH,MODEL_NAME,'frozen_inference_graph.pb')
+PATH_TO_INFERENCE_MODEL = os.path.join(CWD_PATH,MODEL_NAME,'frozen_inference_graph.pb')
 #Path to your label_map:
-PATH_TO_LABELS = os.path.join(CWD_PATH,MODEL_NAME,'label_map.pbtxt')
+PATH_TO_LABELS = os.path.join(CWD_PATH,MODEL_NAME,'labels.txt')
 
 ##############################################
 ## Load the Tensorflow model into new session:
@@ -26,14 +26,14 @@ PATH_TO_LABELS = os.path.join(CWD_PATH,MODEL_NAME,'label_map.pbtxt')
 detection_graph = tf.Graph()
 with detection_graph.as_default():
     od_graph_def = tf.GraphDef()
-    with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+    with tf.gfile.GFile(PATH_TO_INFERENCE_MODEL, 'rb') as fid:
         serialized_graph = fid.read()
         od_graph_def.ParseFromString(serialized_graph)
         tf.import_graph_def(od_graph_def, name='')
     sess = tf.Session(graph=detection_graph)
 
 ######################################################################
-## Get all the output tensors from the model (boxes,class,scores,num):
+## Initiate output tensors from the model (boxes,class,scores,num):
 ######################################################################
 image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
 detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
@@ -83,8 +83,15 @@ while(True):
 
     # Read image using PIL:
     img = Image.open(TEST_IMAGE_PATHS[image_index])
-    # Create drawable from input images:
-    draw = ImageDraw.Draw(img, 'RGBA')
+    try:
+        # Create drawable from input images:
+        draw = ImageDraw.Draw(img, 'RGBA')
+    except:
+        # Compatibility issue with some unsupport format images:
+        print("This image is not supported at this mode, please use classify_image_csv.py to see the output, this can be fixed in time. Thank you")
+        image_index += 1
+        # Make sure the program never quit randomly
+        continue
     # Declare font use for labeling in image:
     font_calibri = ImageFont.truetype("./fonts/Calibri.ttf", size=20)
 
@@ -113,12 +120,13 @@ while(True):
           x_max = right * frame_w
           y_max = top * frame_h
 
-          print('Image ' + str(image_index) + ' : ' + labels[cur_class], 'score = ', cur_score)
+          print('Image ' + str(image_index) + ' : ' + str(labels[cur_class]), 'score = ', cur_score)
 
           box = [x_min, y_min, x_max, y_max]
           # Draw boxes in the image:
           draw_rectangle(draw, box, (0,128,128,20), width=5)
           # Add class name and confidence level into the image:
+          draw.text((10, 10), 'Press D - Next Photo / Press A - Previous Photo', fill=(255,255,255,20))
           draw.text((box[0] + 20, box[1] - 50), labels[cur_class], fill=(255,255,255,20), font=font_calibri)
           draw.text((box[0] + 20, box[1] - 25), str(cur_score), fill=(255,255,255,20), font=font_calibri)
 
@@ -126,8 +134,8 @@ while(True):
     open_cv_image = np.array(img) 
     open_cv_image = open_cv_image[:, :, ::-1].copy() 
     
-    # All the results have been drawn on the frame, so it's time to display it.
-    cv2.imshow('Object detector', open_cv_image)
+    # Images compiled with draw overlay and ready for display:
+    cv2.imshow('Grab Car Classifer', open_cv_image)
 
     while True :
         if cv2.waitKey(1) == ord('a'):
--- car_classifier/object_detection/classify_video.py
+++ car_classifier/object_detection/classify_video.py
@@ -6,12 +6,7 @@ import tensorflow as tf
 import argparse
 import sys
 
-# Set up camera constants
-IM_WIDTH = 1280
-IM_HEIGHT = 720
-#IM_WIDTH = 640    Use smaller resolution for
-#IM_HEIGHT = 480   slightly faster framerate
-
+# Declare input argument parser:
 parser = argparse.ArgumentParser()
 parser.add_argument('--input', type=str, default=0)
 args = parser.parse_args()
@@ -23,84 +18,76 @@ sys.path.append('..')
 from utils import label_map_util
 from utils import visualization_utils as vis_util
 
-# Name of the directory containing the object detection module we're using
-MODEL_NAME = 'trained_models\grab_165k'
-
-# Grab path to current working directory
+##############################
+## Declare requirement inputs:
+##############################
+# Path to your model:
+MODEL_NAME = os.path.join("trained_models","grab_model")
 CWD_PATH = os.getcwd()
-
-# Path to frozen detection graph .pb file, which contains the model that is used
-# for object detection.
-PATH_TO_CKPT = os.path.join(CWD_PATH,MODEL_NAME,'frozen_inference_graph.pb')
-
-# Path to label map file
+# Path to your inference graph:
+PATH_TO_INFERENCE_MODEL = os.path.join(CWD_PATH,MODEL_NAME,'frozen_inference_graph.pb')
+# Path to your label_map:
 PATH_TO_LABELS = os.path.join(CWD_PATH,MODEL_NAME,'label_map.pbtxt')
-
-# Number of classes the object detector can identify
-NUM_CLASSES = 90
+# Number of classes:
+NUM_CLASSES = 196
+# Path to your video input:
+PATH_TO_VIDEO = os.path.join("test_videos",args.input)
 
 ## Load the label map.
-# Label maps map indices to category names, so that when the convolution
-# network predicts `5`, we know that this corresponds to `airplane`.
-# Here we use internal utility functions, but anything that returns a
-# dictionary mapping integers to appropriate string labels would be fine
 label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
 categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
 
-# Load the Tensorflow model into memory.
+##############################################
+## Load the Tensorflow model into new session:
+##############################################
 detection_graph = tf.Graph()
 with detection_graph.as_default():
     od_graph_def = tf.GraphDef()
-    with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+    with tf.gfile.GFile(PATH_TO_INFERENCE_MODEL, 'rb') as fid:
         serialized_graph = fid.read()
         od_graph_def.ParseFromString(serialized_graph)
         tf.import_graph_def(od_graph_def, name='')
 
     sess = tf.Session(graph=detection_graph)
 
-
-# Define input and output tensors (i.e. data) for the object detection classifier
-
-# Input tensor is the image
+######################################################################
+## Initiate output tensors from the model (boxes,class,scores,num):
+######################################################################
 image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
-
-# Output tensors are the detection boxes, scores, and classes
-# Each box represents a part of the image where a particular object was detected
 detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
-
-# Each score represents level of confidence for each of the objects.
-# The score is shown on the result image, together with the class label.
 detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
 detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
-
-# Number of objects detected
 num_detections = detection_graph.get_tensor_by_name('num_detections:0')
 
 # Initialize frame rate calculation
-frame_rate_calc = 1
+frame_rates = 1
 freq = cv2.getTickFrequency()
 font = cv2.FONT_HERSHEY_SIMPLEX
 
-camera = cv2.VideoCapture(args.input)
-ret = camera.set(3,IM_WIDTH)
-ret = camera.set(4,IM_HEIGHT)
+# Initialize camera input:
+camera = cv2.VideoCapture(PATH_TO_VIDEO)
+ret = camera.set(3,1280)
+ret = camera.set(4,720)
 
+################################################
+## Start classification and video playback:
+################################################
 while(True):
+    # Initiate clock (fps):
+    t_start = cv2.getTickCount()
 
-    t1 = cv2.getTickCount()
-
-    # Acquire frame and expand frame dimensions to have shape: [1, None, None, 3]
-    # i.e. a single-column array, where each item in the column has the pixel RGB value
+    # Read video frames into single column RBG array using cv:
     ret, frame = camera.read()
     frame_expanded = np.expand_dims(frame, axis=0)
 
-    # Perform the actual detection by running the model with the image as input
+   # Perform classification based on inference graph and generate correspoding output (boxes,scores,classes,num):
     (boxes, scores, classes, num) = sess.run(
         [detection_boxes, detection_scores, detection_classes, num_detections],
         feed_dict={image_tensor: frame_expanded})
 
-    # Draw the results of the detection (aka 'visulaize the results')
+    # Using Tensorflow Object Detection API to draw the frame rectangle in real-time:
+    # Lower down the min_score_thresh, if its difficult to show any output:
     vis_util.visualize_boxes_and_labels_on_image_array(
         frame,
         np.squeeze(boxes),
@@ -109,18 +96,19 @@ while(True):
         category_index,
         use_normalized_coordinates=True,
         line_thickness=8,
-        min_score_thresh=0.85)
+        min_score_thresh=0.7)
 
-    cv2.putText(frame,"FPS: {0:.2f}".format(frame_rate_calc),(30,50),font,1,(255,255,0),2,cv2.LINE_AA)
+    # Show FPS in video_playback:
+    cv2.putText(frame,"FPS: {0:.2f}".format(frame_rates),(30,50),font,1,(255,255,0),2,cv2.LINE_AA)
     
-    # All the results have been drawn on the frame, so it's time to display it.
-    cv2.imshow('Object detector', frame)
+    # Images compiled with draw overlay and ready for display:
+    cv2.imshow('Grab Car Classifier', frame)
 
-    t2 = cv2.getTickCount()
-    time1 = (t2-t1)/freq
-    frame_rate_calc = 1/time1
+    t_end = cv2.getTickCount()
+    time_1 = (t_end-t_start)/freq
+    frame_rates = 1/time_1
 
-    # Press 'q' to quit
+    # Press 'q' to quit program:
     if cv2.waitKey(1) == ord('q'):
         break
 
